f3cc9bb5944132ecbb1e24c13cdb78de09d2f150
chintusravan431 <chintu.sravan431@gmail.com>
2019-12-23 07:02:27 +0530

    Adding project

---
+++ Project/src/com/icici/First.java
@@ -0,0 +1,14 @@
+package com.icici;
+
+public class First {
+
+	public static void main(String[] args)
+	{
+		
+	int a=1, b=5, sum;
+	sum=a+b;
+	System.out.println("sum of two number:" +sum); 
+
+	}
+
+}
